4b2c3be6b41596aca586a4a3eab59124de271e7a
CaioHenriqueCastro <caiohenriqueestevesdecastro@gmail.com>
2023-02-22 22:05:30 -0300

    ok

---
+++ produto.js
@@ -0,0 +1,18 @@
+function calcular (){
+    validaDesc()
+    let nTotal = quant.value * unit.value;
+    let item = desc.value
+    total.value = nTotal;
+    console.log(nTotal, item)
+}
+
+function validaDesc (){
+    let item = desc.value
+    if(item.trim() === ""){
+        desc.style.background = "yellow";
+        mensagem.innerHTML = "Preencha a descrição do produto!";
+        $('#alerta').modal('show');
+    }else {
+        desc.style.background = 'white';
+    }
+}
